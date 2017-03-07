1249da8bdc916c9df8d9be3ae10a8ce13f37a8c7
13608089849 <1213814232@qq.com>
2017-03-07 16:53:14 +0800

    Adjust layout

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
@@ -8,8 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 
@@ -21,79 +18,77 @@ public class MainFrame extends JFrame {
 
 		panel.setLayout(null);
 
-		JLabel lable_Title = new JLabel("File Filter",JLabel.CENTER);
+		JLabel lable_Title = new JLabel("File Filter", JLabel.CENTER);
 		lable_Title.setBounds(350, 10, 100, 50);
 		panel.add(lable_Title);
-		
-		JLabel lable_Folder = new JLabel("Folder:",JLabel.LEFT);
-		lable_Folder.setBounds(20,60,50,20);
+
+		JLabel lable_Folder = new JLabel("Folder:", JLabel.LEFT);
+		lable_Folder.setBounds(20, 60, 50, 20);
 		panel.add(lable_Folder);
-		
+
 		JTextField textField_Folder = new JTextField();
-		textField_Folder.setBounds(80,60,200,20);
+		textField_Folder.setBounds(80, 60, 200, 20);
 		panel.add(textField_Folder);
 
 		JButton button_Choose = new JButton("Choose");
-		button_Choose.setBounds(300,60,80,20);
+		button_Choose.setBounds(300, 60, 80, 20);
 		panel.add(button_Choose);
-		
-		JLabel lable_Fromat = new JLabel("Fromat:",JLabel.CENTER);
-		lable_Fromat.setBounds(430,60,50,20);
+
+		JLabel lable_Fromat = new JLabel("Fromat:", JLabel.CENTER);
+		lable_Fromat.setBounds(430, 60, 50, 20);
 		panel.add(lable_Fromat);
-		
+
 		JTextField textField_Format = new JTextField();
 		textField_Format.setBounds(500, 60, 200, 20);
 		panel.add(textField_Format);
-		
-		JLabel lable_Operation = new JLabel("Operation Mode:",JLabel.LEFT);
-		lable_Operation.setBounds(20,100,100,20);
+
+		JLabel lable_Operation = new JLabel("Operation Mode:", JLabel.LEFT);
+		lable_Operation.setBounds(20, 100, 100, 20);
 		panel.add(lable_Operation);
-		
+
 		JComboBox combox_Operation = new JComboBox();
 		combox_Operation.addItem("Automate");
 		combox_Operation.addItem("Manual");
-		combox_Operation.setBounds(140,100,80,20);
+		combox_Operation.setBounds(140, 100, 100, 20);
 		panel.add(combox_Operation);
-		
-		
-		JLabel lable_Process = new JLabel("Process Mode:",JLabel.LEFT);
-		lable_Process.setBounds(270,100,100,20);
+
+		JLabel lable_Process = new JLabel("Process Mode:", JLabel.LEFT);
+		lable_Process.setBounds(270, 100, 100, 20);
 		panel.add(lable_Process);
-		
-		
+
 		JComboBox combox_Process = new JComboBox();
 		combox_Process.addItem("Violent");
 		combox_Process.addItem("Soft");
-		combox_Process.setBounds(390,100,80,20);
+		combox_Process.setBounds(390, 100, 100, 20);
 		panel.add(combox_Process);
-		
+
 		JButton button_Filter = new JButton("Filter");
-		button_Filter.setBounds(600,100,80,20);
+		button_Filter.setBounds(580, 100, 80, 20);
 		panel.add(button_Filter);
-		
+
 		JButton button_Stop = new JButton("Stop");
-		button_Stop.setBounds(700,100,80,20);
+		button_Stop.setBounds(680, 100, 80, 20);
 		panel.add(button_Stop);
-		
+
 		Vector<String> vector_Matched = new Vector<String>();
 		vector_Matched.add("File 1");
 		vector_Matched.add("File 2");
 		vector_Matched.add("File 3");
-        JList list_Matched = new JList(vector_Matched);
-        list_Matched.setBorder(BorderFactory.createTitledBorder("Matched:"));
-        list_Matched.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list_Matched.setBounds(10,180,360,380);
-        panel.add(list_Matched);
-        
-        Vector<String> vector_Unmatched = new Vector<String>();
-        vector_Unmatched.add("File 1");
-        vector_Unmatched.add("File 2");
-        vector_Unmatched.add("File 3");
-        JList list_Unmatched = new JList(vector_Unmatched);
-        list_Unmatched.setBorder(BorderFactory.createTitledBorder("Unmatched:"));
-        list_Unmatched.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list_Unmatched.setBounds(410,180,360,380);
-        panel.add(list_Unmatched);
+		JList list_Matched = new JList(vector_Matched);
+		list_Matched.setBorder(BorderFactory.createTitledBorder("Matched:"));
+		list_Matched.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list_Matched.setBounds(10, 160, 360, 400);
+		panel.add(list_Matched);
+
+		Vector<String> vector_Unmatched = new Vector<String>();
+		vector_Unmatched.add("File 1");
+		vector_Unmatched.add("File 2");
+		vector_Unmatched.add("File 3");
+		JList list_Unmatched = new JList(vector_Unmatched);
+		list_Unmatched.setBorder(BorderFactory.createTitledBorder("Unmatched:"));
+		list_Unmatched.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list_Unmatched.setBounds(410, 160, 360, 400);
+		panel.add(list_Unmatched);
 	}
 
 	public static void main(String[] args) {
